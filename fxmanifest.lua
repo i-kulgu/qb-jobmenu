00078f7b18d344f120b4dddbb47be56f44f0da4c
i-kulgu <i.kulgu@gmail.com>
2022-06-03 16:36:34 +0200

    initial

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,8 +7,8 @@ version '1.0.0'
 
 shared_scripts {
     '@qb-core/shared/locale.lua',
+    'locales/en.lua',
     'config.lua',
-    'locales/en.lua' -- Change this to your preferred language
 }
 
 client_scripts {
