a11482b72443a34ea755257652d648f237fc5dd1
i-kulgu <i.kulgu@gmail.com>
2022-06-07 09:43:01 +0200

    initial

--- locales/en.lua
+++ locales/en.lua
@@ -1,6 +1,6 @@
 local Translations = {
     error = {
-        not_whitelistesJob = "You need to be in a whitelisted job to use this menu",
+        not_whitelistedJob = "You need to be in a whitelisted job to use this menu",
         not_onDuty = "You need to be on duty to use this menu",
     },
     menu = {
